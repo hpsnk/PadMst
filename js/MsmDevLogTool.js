7d4fb0788ba9ff8e4ed56474dcda158fb2ecdcf5
happysonic <archer03371@163.com>
2021-12-11 19:37:49 +0800

    logtool

---
+++ js/MsmDevLogTool.js
@@ -0,0 +1,32 @@
+
+$(function(){
+
+  //radio-namespace
+  $('input:radio[name="input_namesapce"]').change(function() {
+    setAllCmd();
+  });
+  
+  //radio-service
+  $('input:radio[name="input_service"]').change(function() {
+    setAllCmd();
+  });
+
+  setAllCmd();
+
+});
+
+
+function setAllCmd() {
+
+  var valNamespace = $('input:radio[name="input_namesapce"]:checked').val();
+  console.log("namespace=" + valNamespace);
+
+  var valService = $('input:radio[name="input_service"]:checked').val();
+  console.log("service=" + valService);
+  
+  if (valNamespace!=undefined && valService!=undefined) {
+    var cmdListPod = "kubectl get pods -n namespace-a-m-tw-" + valNamespace + "-01 | grep 'deployment-" + valService + "01'";
+    $("#input_listCmd").val(cmdListPod);
+  }
+
+}
